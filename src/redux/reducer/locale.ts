79aa61fc5b59e766febaaff4caca53356364646d
maxyou <hyyou2010@gmail.com>
2019-08-22 18:45:49 +0800

    words undefined

--- src/redux/reducer/locale.ts
+++ src/redux/reducer/locale.ts
@@ -4,39 +4,42 @@ import zh from 'locale/zh-cn'
 
 var moment = require('moment');
 
-function mapLanguage(language){
-    const code = language.substring(0,2)
-    console.log('mapLanguage:'+code)
-    switch (code){
+function mapLanguage(language) {
+    const code = language.substring(0, 2)
+    console.log('mapLanguage:' + code)
+    switch (code) {
         case 'en':
-            moment.locale('en-us');                 
+            moment.locale('en-us');
             return en
         case 'zh':
-            moment.locale('zh-cn');                 
+            moment.locale('zh-cn');
             return zh
+        default:
+            moment.locale('en-us');
+            return en
     }
 }
 interface IState {
     language: string,
     words: object
 }
-const initState:IState = {
+const initState: IState = {
     language: 'zh',
     words: zh
 }
 moment.locale('zh-cn')
 
-export default function locale(state:IState = initState, action:{type:string, payload:any}):IState {
-    
+export default function locale(state: IState = initState, action: { type: string, payload: any }): IState {
+
     switch (action.type) {
 
         case actionLocale.ACTION.LANGUAGE_SET:
             console.log('reducer: after locale set')
-            console.log(action.payload)       
-            return { ...state, language: action.payload, words: mapLanguage(action.payload)}
+            console.log(action.payload)
+            return { ...state, language: action.payload, words: mapLanguage(action.payload) }
         case actionLocale.ACTION.LANGUAGE_RESET:
             console.log('reducer: after locale reset')
-            return { ...initState}
+            return { ...initState }
 
         default:
             return state
